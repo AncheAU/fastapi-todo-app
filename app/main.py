2ee9bf47759954dfdee08666f7ff3bf154ea32f5
AncheAU <auanche333@gmail.com>
2026-06-02 14:37:36 +0100

    Added new endpoint get  health

--- app/main.py
+++ app/main.py
@@ -2,15 +2,48 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+#Todos
+todos = [
+    {
+        "id": 1,
+        "title": "Learn FastAPI",
+        "completed": False
+    },
+    {
+        "id": 2,
+        "title": "Learn Git",
+        "completed": False
+    },
+    {
+        "id": 3,
+        "title": "Learn uv",
+        "completed": True
+    }
+    
+]
+
+
 @app.get("/")
 def home():
     return {
         "message": "Welcome to Abdullahi todo list."
     }
 
+
 @app.get("/about")
 def about():
     return {
         "app": "Todo API",
         "version": "1.0"
+    }
+
+@app.get("/todos")
+def get_todos():
+    return todos
+
+
+@app.get("/health")
+def get_health():
+    return {
+        "status": "healthy"
     }
